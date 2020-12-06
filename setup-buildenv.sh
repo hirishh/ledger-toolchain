b1ab11fd42f4faa764c84c267ccca0da676fc3dd
Matteo Ferrari <matteo.ferrari.1@cern.ch>
2020-12-06 19:38:10 +0100

    update

--- setup-buildenv.sh
+++ setup-buildenv.sh
@@ -10,17 +10,19 @@ if [ ! -d bolos_env/clang-arm-fropi ]; then
 
   mkdir bolos_env
 
-  cd bolos_env
-  wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
-  bunzip2 gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2
-  tar xvf gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar
-  rm -f gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar
+  cd bolos_env || exit
+  wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
+  bunzip2 gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
+  tar xvf gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar
+  rm -f gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar
 
-  wget http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-  tar xvf clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-  rm -f clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+  ln -sf gcc-arm-none-eabi-9-2020-q2-update gcc-arm-none-eabi
 
-  ln -sf clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04 clang-arm-fropi 
+  wget https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+  tar xvf clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+  rm -f clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+
+  ln -sf clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04 clang-arm-fropi
 
   cd ..
 
